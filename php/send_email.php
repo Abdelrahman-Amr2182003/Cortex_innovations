efee31bfff385b1bb3b3b6aa9195b42741fde463
abdelrahman amr <69196484+Abdelrahman-Amr2182003@users.noreply.github.com>
2023-09-26 08:08:39 +0300

    Add files via upload

--- php/send_email.php
+++ php/send_email.php
@@ -1,28 +1,49 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $recipient_email = "abdelrahmanamr2182003@gmail.com"; // Change to your recipient's email address
-
+    // Extract form data from POST request
     $name = $_POST["name"];
-    $email = $_POST["email"];
     $subject = $_POST["subject"];
+    $email = $_POST["email"];
     $message = $_POST["message"];
 
-    $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+    // Configure your SMTP settings (for Gmail)
+    $smtpUsername = 'your_email@gmail.com';
+    $smtpPassword = 'your_email_password';
 
-    if (mail($recipient_email, $subject, $message, $headers)) {
-        // Email sent successfully, redirect with success status
-        header("Location: ../contact/contact.html?status=success");
-        exit(); // Stop script execution after redirect
+    // Create a new PHPMailer instance
+    require('PHPMailer/src/PHPMailer.php');
+    require('PHPMailer/src/SMTP.php');
+    require('PHPMailer/src/Exception.php');
+
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    $mail->IsSMTP();
+    $mail->CharSet = 'UTF-8';
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPDebug = 2; // Enable verbose debugging
+    $mail->Debugoutput = 'html'; // Display debug output as HTML
+    $mail->Port = 587; // 465 or 587
+    $mail->SMTPSecure = 'tls';
+    $mail->SMTPAuth = true;
+    $mail->IsHTML(true);
+
+    // Sender (From) and Recipient (To) email addresses
+    $mail->setFrom('your_email@gmail.com', 'Your Name'); // Use your email as the sender
+    $mail->addAddress('company_email@example.com'); // Company email
+
+    // Set the "Reply-To" header to the user's email
+    $mail->addReplyTo($email, $name);
+
+    // Email subject and content
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+
+    // Send email and check for success
+    if ($mail->send()) {
+        echo json_encode(array("message" => "success"));
     } else {
-        // Email could not be sent, redirect with error status
-        header("Location: ../contact/contact.html?status=error");
-        exit(); // Stop script execution after redirect
+        echo json_encode(array("message" => "error"));
     }
 } else {
-    // Redirect back to the contact form if accessed without POST data
-    header("Location: ../contact/contact.html");
-    exit(); // Stop script execution after redirect
+    echo json_encode(array("message" => "Invalid request method."));
 }
 ?>
