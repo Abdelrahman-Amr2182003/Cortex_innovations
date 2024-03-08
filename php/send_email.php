4ceb701f5f04b5faf5b3761f7ccbd57899fd7a0d
abdelrahman amr <abdelrahmanamr2182003@gmail.com>
2024-03-08 21:11:49 +0200

    Initial commit

---
+++ php/send_email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $recipient_email = "contact@cortex-innovations.com";
+    $from_email = $_POST["Email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["Message"];
+    
+    $headers = "From: $from_email";
+
+    if (mail($recipient_email, $subject, $message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Email sending failed.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
